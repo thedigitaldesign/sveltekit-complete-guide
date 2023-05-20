900dda930eced6370d6bb3a1ce1d88d9e995b2bc
Joshua Christensen <joshua@codingwithjosh.com>
2023-05-20 12:12:46 -0500

    Section 2: 10|32

---
+++ src/components/ColorSlider.svelte
@@ -0,0 +1,20 @@
+<script type="ts">
+  export let colorName = 'red'
+  export let colorValue = 100
+</script>
+
+<label for="{colorName}Slider">{colorName} ({colorValue})</label>
+<input type="range" name="{colorName}Slider" bind:value={colorValue} min="0" max="255" />
+
+<style>
+  label {
+    display: block;
+    font-size: 20px;
+    font-weight: normal;
+    text-transform: capitalize;
+  }
+
+  input {
+    width: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-	import Fun from "../components/Fun.svelte"
+	import ColorSlider from "../components/ColorSlider.svelte"
+import Fun from "../components/Fun.svelte"
+
+  let name = 'Rebekah'
+  let fontSize = 16
+
+  let red = 100
+  let green = 100
+  let blue = 100
 </script>
 
 <div>
@@ -9,8 +17,45 @@
 
 <Fun />
 
+<h2>My name is sezy {name}</h2>
+
+<input type="text" name="name" bind:value={name} />
+
+<br>
+<br>
+<br>
+
+<input type="range" name="fontSize" bind:value={fontSize} />
+<h2 style="font-size: {fontSize}px;">Font Size: {fontSize}</h2>
+
+<div id="color-slider">
+  <ColorSlider colorName="red" bind:colorValue={red} />
+  <ColorSlider colorName="green" bind:colorValue={green} />
+  <ColorSlider colorName="blue" bind:colorValue={blue} />
+
+  <div class="color-swatch" style="background-color: rgb({red}, {green}, {blue});"></div>
+
+  <p style="font-size: 30px; text-align: center;">rgb ({red}, {green}, {blue})</p>
+</div>
+
 <style lang="less">
 	h1 {
 		color: red;
 	}
+
+  #color-slider {
+    width: 100%;
+    
+    input {
+      width: 100%;
+    }
+
+    .color-swatch {
+      border: 4px dashed black;
+      width: 100%;
+      height: 200px;
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+  }
 </style>
