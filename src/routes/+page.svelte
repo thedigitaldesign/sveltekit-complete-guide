695abae33429bbe2091f18879ea4d586a0a5b268
Joshua Christensen <joshua@codingwithjosh.com>
2023-05-19 20:30:50 -0500

    Section 2: 6|32

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script type="ts">
+	import Fun from '../components/Fun.svelte'
+
+	let name = 'hallo Rebekah'
+	let favoriteColor = 'pink'
+  let favoriteNumber = 68
+
+	setTimeout(() => {
+		favoriteColor = 'blue'
+    favoriteNumber++
+	}, 1000)
+</script>
+
+<Fun />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p style="color: {favoriteColor}">{name}</p>
+<p>Her favorite number is: {favoriteNumber}</p>
