f98395a04f1c5c9b93a524dda1c870b784379075
Joshua Christensen <joshua@codingwithjosh.com>
2023-05-19 23:11:30 -0500

    Section 2: 9|32

--- src/components/Fun.svelte
+++ src/components/Fun.svelte
@@ -1,1 +1,8 @@
-<h2>You rock and roll!</h2>
+<script lang="ts">
+	export let fontSize = 16
+</script>
+
+<div style="font-size: {fontSize}px">
+	<h1>Fun Component</h1>
+	<p>Fun Component stuff goes here</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,16 @@
-<script type="ts">
-	import Fun from '../components/Fun.svelte'
-
-	let name = 'hallo Rebekah'
-	let favoriteColor = 'pink'
-  let favoriteNumber = 68
-
-	setTimeout(() => {
-		favoriteColor = 'blue'
-    favoriteNumber++
-	}, 1000)
+<script lang="ts">
+	import Fun from "../components/Fun.svelte"
 </script>
+
+<div>
+	<h1>Fun</h1>
+	<p>Fun stuff goes here</p>
+</div>
 
 <Fun />
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<p style="color: {favoriteColor}">{name}</p>
-<p>Her favorite number is: {favoriteNumber}</p>
+<style lang="less">
+	h1 {
+		color: red;
+	}
+</style>
