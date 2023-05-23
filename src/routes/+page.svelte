8c369e93a4692d0d31559a038f1c2ae8741c2014
Joshua (Josh) Christensen <joshua@thedigitaldesign.com>
2023-05-22 19:28:36 -0500

    Section 2: 13|32 - Reactive Statements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
-	import FormContainer from '../components/FormContainer.svelte'
+  let name = 'world';
+  $: greeting = `Hello ${name.toUpperCase()}!`;
 
-  const onFormSubmit = (event: CustomEvent) => {
-    console.log(event.detail)
+  let number = 55
+  $: dogYears = number * 7
 
-    const { name, number } = event.detail
+  const color = (red: number, green: number, blue: number) => {
+    return `rgb(${red}, ${green}, ${blue})`
+  }
 
-    console.log(name, number, 'from +page.svelte')
+  let red = 255
+  let green = 0
+  let blue = 127
+
+  $: colorValue = color(red, green, blue)
+
+  setTimeout(() => {
+    red = 0
+    green = 255
+    blue = 255
+  }, 1500);
+
+  $: console.log('colorValue', colorValue)
+  $: {
+    console.log('red: ', red)
+    console.log('green: ', green)
+    console.log('blue: ', blue)
   }
 </script>
 
 <div>
-	<FormContainer on:form_submit={onFormSubmit} />
+  <label for="name">Name</label>
+  <input type="text" name="name" bind:value={name} />
+  <h2>Name: {name}</h2>
+  <h3>{greeting}</h3>
+
+  <label for="number">Number</label>
+  <input type="text" name="number" bind:value={number} />
+  <h2>Number: {number}</h2>
+  <h2>Dog Years: {dogYears}</h2>
+
+  <h2 style="color: {colorValue}">{name} with a side of {number}</h2>
 </div>
