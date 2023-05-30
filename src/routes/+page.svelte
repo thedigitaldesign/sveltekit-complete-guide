643a821e3e99588cf3afd1f3024d331f6464d5fa
Joshua Christensen <joshua@codingwithjosh.com>
2023-05-29 21:15:23 -0500

    Section 2: 16|32 Each Statements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,18 @@
 <script lang="ts">
-  let name = 'world';
-  $: greeting = `Hello ${name.toUpperCase()}!`;
+	let colorList: string[] = ['red', 'green', 'blue', 'purple']
+  let newColor: string = ''
 
-  let number = 55
-  $: dogYears = number * 7
-
-  const color = (red: number, green: number, blue: number) => {
-    return `rgb(${red}, ${green}, ${blue})`
-  }
-
-  let red = 255
-  let green = 0
-  let blue = 127
-
-  $: colorValue = color(red, green, blue)
-
-  setTimeout(() => {
-    red = 0
-    green = 255
-    blue = 255
-  }, 1500);
-
-  $: console.log('colorValue', colorValue)
-  $: {
-    console.log('red: ', red)
-    console.log('green: ', green)
-    console.log('blue: ', blue)
+  const addColor = () => {
+    colorList = [...colorList, newColor]
+    newColor = ''
   }
 </script>
 
-<div>
-  <label for="name">Name</label>
-  <input type="text" name="name" bind:value={name} />
-  <h2>Name: {name}</h2>
-  <h3>{greeting}</h3>
+<select name="colorListSelect">
+  {#each colorList as color}
+    <option value={color}>{color}</option>
+  {/each}
+</select>
 
-  <label for="number">Number</label>
-  <input type="text" name="number" bind:value={number} />
-  <h2>Number: {number}</h2>
-  <h2>Dog Years: {dogYears}</h2>
-
-  <h2 style="color: {colorValue}">{name} with a side of {number}</h2>
-</div>
+<input type="text" name="colorListAdd" bind:value={newColor} />
+<button on:click={addColor}>Add Color</button>
