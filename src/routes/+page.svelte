47e043d89fc814c842ab2d2a72568d4bcf2ea27a
Joshua Christensen <joshua@codingwithjosh.com>
2023-05-29 22:13:13 -0500

    Section 2: 16|32 Each Statements Pt. 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	let colorList: string[] = ['red', 'green', 'blue', 'purple']
-  let newColor: string = ''
+	let newColor: string = ''
 
-  const addColor = () => {
-    colorList = [...colorList, newColor]
-    newColor = ''
-  }
+	const addColor = () => {
+		if (!newColor) return
+    
+		colorList = [...colorList, newColor]
+		newColor = ''
+	}
 </script>
 
 <select name="colorListSelect">
-  {#each colorList as color}
-    <option value={color}>{color}</option>
-  {/each}
+	{#each colorList as color}
+		<option value={color}>{color}</option>
+	{/each}
 </select>
 
 <input type="text" name="colorListAdd" bind:value={newColor} />
